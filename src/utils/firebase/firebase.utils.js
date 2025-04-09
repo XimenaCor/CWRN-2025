89c11eec2c49035a218b4efc145721b865b2c58a
Ximena Cordero <cor.xime@gmail.com>
2025-04-08 23:02:52 -0400

    CRWN-2025: Created sign up, form input and button component

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,46 +1,60 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword
+} from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBtkuWz9Otgd9f1GuSxsM5lYyk-5jlVw6Q",
-    authDomain: "crwn-db-2025.firebaseapp.com",
-    projectId: "crwn-db-2025",
-    storageBucket: "crwn-db-2025.firebasestorage.app",
-    messagingSenderId: "860029537526",
-    appId: "1:860029537526:web:8f6d9650ab7b9f6122f0d7"
+  apiKey: "AIzaSyBtkuWz9Otgd9f1GuSxsM5lYyk-5jlVw6Q",
+  authDomain: "crwn-db-2025.firebaseapp.com",
+  projectId: "crwn-db-2025",
+  storageBucket: "crwn-db-2025.firebasestorage.app",
+  messagingSenderId: "860029537526",
+  appId: "1:860029537526:web:8f6d9650ab7b9f6122f0d7"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
-export const db = getFirestore(firebaseApp);
+export const db = getFirestore();
 
-export const createUserDocumentFromAuth = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+  if (!userAuth) return;
 
-    const userDocRef = doc(db, 'users', userAuth.uid);
-    const userSnapshot = await getDoc(userDocRef);
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
 
-    if (!userSnapshot.exists()) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-        try {
-            await setDoc(userDocRef, {
-                displayName,
-                email,
-                createdAt
-            });
-        } catch (error) {
-            console.error('Error creating user', error.message);
-        }
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation
+      });
+    } catch (error) {
+      console.error('Error creating user', error.message);
     }
+  }
 
-    return userDocRef;
+  return userDocRef;
 };
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
